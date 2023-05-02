7b82e1138bfd7d5e07c6d846af2bf8c21e73bb59
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-05-03 01:24:51 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3000; 
+
+const API_KEY = process.env.API_KEY;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/summarize", async (req, res) => {
+    const { bookTitle } = req.body;
+  
+    try {
+      const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: `summarize the book: ${bookTitle} in details and explain the main ideas behind each `, 
+            },
+          ],
+        }),
+      });
+  
+      const data = await response.json();
+  
+     
+        res.json(data.choices[0].message.content);
+     
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while processing your request" });
+    }
+  });
+  
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+ 
