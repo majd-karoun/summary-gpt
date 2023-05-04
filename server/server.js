8272e8e5cfc155affca609f21d1af56ce81a0bbe
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-05-04 11:53:11 +0200

    updated summarizing logic

--- server/server.js
+++ server/server.js
@@ -3,17 +3,22 @@ const app = express();
 const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args));
 require("dotenv").config();
 const cors = require("cors");
+const bodyParser = require('body-parser');
+
+
 
 const PORT = process.env.PORT || 3000; 
 
 const API_KEY = process.env.API_KEY;
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/api/summarize", async (req, res) => {
     const { bookTitle } = req.body;
     const { language } = req.body;
+    console.log(req.body)
   
     try {
       const response = await fetch("https://api.openai.com/v1/chat/completions", {
@@ -27,7 +32,7 @@ app.post("/api/summarize", async (req, res) => {
           messages: [
             {
               role: "user",
-              content: `summarize the book: ${bookTitle}in details and explain the main ideas behind each and translate it to ${language} if it is in a different language.`, 
+              content: `respond to me in ${language}. summarize the book: ${bookTitle} in details and explain the main ideas behind each section of the book`, 
             },
           ],
         }),
