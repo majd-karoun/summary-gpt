a1114a5bf837d14d8272803e9e30e4bfdaa89def
Majd Karoun <107414000+majd-karoun@users.noreply.github.com>
2023-05-04 14:50:37 +0200

    summarizing logic updated

--- server/server.js
+++ server/server.js
@@ -32,7 +32,8 @@ app.post("/api/summarize", async (req, res) => {
           messages: [
             {
               role: "user",
-              content: `respond in ${language}. summarize the book: ${bookTitle} in details and explain the main ideas behind each section of the book. If the summary was not possible start responding with SummaryError: `, 
+              content: `((respond in ${language}) summarize the book: "${bookTitle}" in ${language} and in details and explain the main ideas behind each section of the book. (only if you faced an error before you finish start your response with "SummaryError:")`
+              , 
             },
           ],
         }),
